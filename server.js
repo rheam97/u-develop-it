7ef2af723a79d8ccdcccd52d723597b3f2a431bf
Rhea <rheam97@gmail.com>
2022-02-07 13:02:49 -0500

    created database query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -5,7 +6,17 @@ const app = express()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'election'
+},
+console.log('Connected to the election database'))
 
+db.query(`SELECT * FROM candidates`, (err, rows)=> {
+console.log(rows)
+})
 app.use((req, res)=> {
     res.status(404).end()
 })
