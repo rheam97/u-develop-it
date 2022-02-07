ec07951b8faf7b50397091c6fa6a85de4550d81a
Rhea <rheam97@gmail.com>
2022-02-07 14:46:28 -0500

    added parties table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
