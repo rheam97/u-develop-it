316a47101ce93e32639a4b32cf0c09f042e4b9a1
Rhea <rheam97@gmail.com>
2022-02-07 13:56:51 -0500

    made routes to database queries

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2')
-const express = require('express')
+const inputCheck = require('./utils/inputCheck')
+const express = require('express');
+const { result } = require('lodash');
 const PORT = process.env.PORT || 3001
 const app = express()
 // Express middleware
@@ -14,6 +16,77 @@ const db = mysql.createConnection({
 },
 console.log('Connected to the election database'))
 
+app.get('/api/candidates', (req, res)=> {
+    const sql = `SELECT * FROM candidates`
+    db.query(sql, (err, rows)=> {
+        if(err){
+            res.status(500).json({error: err.message})
+            return
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
+})
+
+app.get('/api/candidates/:id', (req, res)=> {
+    const sql = `SELECT * FROM CANDIDATES WHERE id =?`
+    const params = req.params.id
+    db.query(sql, params, (err, rows)=> {
+        if(err){
+            res.status(400).json({error:err.message})
+            return
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
+})
+
+app.delete('/api/candidates/:id', (req, res)=> {
+    const sql = `DELETE FROM candidates WHERE id =?`
+    const params = [req.params.id]
+
+    db.query(sql, params, (err, result)=> {
+        if(err){
+            res.statusMessage(400).json({error: res.message})
+        }else if(!result.affectedRows){
+            res.json({
+                message: 'Candidate not found.'
+            })
+        }
+        else{
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+    })
+})
+
+app.post(`/api/candidates`, ({body}, res)=> {
+    const error = inputCheck(body, 'first_name', 'last_name', 'industry_connected')
+    if(error){
+        res.status(400).json({error: error})
+        return
+    }
+    const sql = `INSERT INTO candidates(first_name, last_name, industry_connected)
+    VALUES (?,?,?)`
+    const params = [body.first_name, body.last_name, body.industry_connected]
+    db.query(sql, params, (err, result)=> {
+        if(err){
+            res.status(400).json({error: err.message})
+            return
+        }
+        res.json({
+            message: 'succes',
+            data: body
+        })
+    })
+})
 // db.query(`SELECT * FROM candidates WHERE id=1`, (err, rows)=> {
 //     if(err){
 //         console.log(err)
