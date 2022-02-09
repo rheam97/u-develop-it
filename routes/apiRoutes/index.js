f498a5a271b7c392a79a3b82380cab25b2e60176
Rhea <rheam97@gmail.com>
2022-02-09 17:08:16 -0500

    tallied votes and made route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = require('express').Router()
 router.use(require('./candidates'))
 router.use(require('./parties'))
 router.use(require('./voters'))
+router.use(require('./votes'))
 
 module.exports= router
